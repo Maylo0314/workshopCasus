4188532d836dc8ceb15531a7643407d1eb32356a
Maylo0314 <maylo.elfrink@student.graafschapcollege.nl>
2023-06-07 13:17:51 +0200

    knoppen aan Registreren en index

--- public/Registreren.php
+++ public/Registreren.php
@@ -40,6 +40,11 @@
         <br>
     <button type="submit" href="index.php" class="btn btn-primary buttonkleur" name="login">klant aanmaken</button>
 
+    <br>
+    <br>
+
+    <a href="Index.php" class="btn btn-primary buttonkleur">Bijhoud pagina</a>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
 </html> 
